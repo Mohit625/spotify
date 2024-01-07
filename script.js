7251c9f39506c65b9d9140ef323643abc2dd7de7
Mohit625 <154047660+Mohit625@users.noreply.github.com>
2024-01-07 22:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesAndSeconds(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`;
   }
 async function getsongs(){
-let a = await fetch("/songs/");
+let a = await fetch("songs/");
 let response = await a.text();
 let div = document.createElement("div");
 div.innerHTML = response;

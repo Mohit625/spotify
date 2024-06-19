655a6ab8a7ec90ec0489565f0f7146072e6ccb0d
Mohit625 <154047660+Mohit625@users.noreply.github.com>
2024-06-19 19:04:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function secondsToMinutesAndSeconds(seconds) {
   }
 async function getsongs(folder){
 currfolder = folder;
-let a = await fetch(`/${folder}/`);
+let a = await fetch(`https://mohit625.github.io/spotify/${folder}/`);
 let response = await a.text();
 let div = document.createElement("div");
 div.innerHTML = response;
@@ -60,7 +60,7 @@ const playmusic = (track, pause = false)=>{
     time.innerHTML = "";
 }
 async function main(){
-s =  await getsongs("library/Chillout/Bana Sharabi_PagalWorld.com.pe_.mp3");
+s =  await getsongs("library/Chillout");
     playmusic(s[0], true);
 masterplay.addEventListener("click" ,()=>{
     if(currsong.paused){

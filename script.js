3f971c2688ab8615a896e3fa784e1c373badb18e
Mohit625 <154047660+Mohit625@users.noreply.github.com>
2024-06-19 14:18:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ const playmusic = (track, pause = false)=>{
     time.innerHTML = "";
 }
 async function main(){
-s =  await getsongs("library/Chillout");
+s =  await getsongs("library/Happy");
     playmusic(s[0], true);
 masterplay.addEventListener("click" ,()=>{
     if(currsong.paused){
